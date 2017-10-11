9159412fc7a33415e1f7b70d8d3a3533cf4e9e84
Dell-PC\Dell <sameer@aayati.co.in>
2017-10-11 20:23:59 +0530

    Initial Commit

---
+++ views/sg-test/_form.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\helpers\Html;
+use kartik\widgets\ActiveForm;
+use kartik\builder\Form;
+use kartik\datecontrol\DateControl;
+
+/**
+ * @var yii\web\View $this
+ * @var app\models\SgTest $model
+ * @var yii\widgets\ActiveForm $form
+ */
+?>
+
+<div class="sg-test-form">
+
+    <?php $form = ActiveForm::begin(['type' => ActiveForm::TYPE_HORIZONTAL]); echo Form::widget([
+
+        'model' => $model,
+        'form' => $form,
+        'columns' => 1,
+        'attributes' => [
+
+            'Name' => ['type' => Form::INPUT_TEXT, 'options' => ['placeholder' => 'Enter Test...']],
+
+            'date' => ['type' => Form::INPUT_WIDGET, 'widgetClass' => DateControl::classname(),'options' => ['type' => DateControl::FORMAT_DATETIME]],
+
+            'mod_date' => ['type' => Form::INPUT_WIDGET, 'widgetClass' => DateControl::classname(),'options' => ['type' => DateControl::FORMAT_DATETIME]],
+
+        ]
+
+    ]);
+
+    echo Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'),
+        ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']
+    );
+    ActiveForm::end(); ?>
+
+</div>
---
+++ views/sg-test/_search.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/**
+ * @var yii\web\View $this
+ * @var app\models\SgTestSearch $model
+ * @var yii\widgets\ActiveForm $form
+ */
+?>
+
+<div class="sg-test-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'Name') ?>
+
+    <?= $form->field($model, 'date') ?>
+
+    <?= $form->field($model, 'mod_date') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
